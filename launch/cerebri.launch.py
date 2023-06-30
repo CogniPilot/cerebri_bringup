014f41d05cbacb49ace922905441773fae7ba440
James Goppert <james.goppert@gmail.com>
2023-06-30 06:46:26 +0000

    Add gdb flag.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- launch/cerebri.launch.py
+++ launch/cerebri.launch.py
@@ -8,24 +8,24 @@ from launch.substitutions import LaunchConfiguration
 def launch_cerebri(context, *args, **kwargs):
 
     uart_shell = LaunchConfiguration('uart_shell').perform(context)
-    debugger = LaunchConfiguration('debugger').perform(context)
+    gdb = LaunchConfiguration('gdb').perform(context)
     vehicle = LaunchConfiguration('vehicle').perform(context)
 
     cmd = f"cerebri_{vehicle}_native_posix"
     xterm_cmd = 'xterm -fa Monospace -fs 12 -fg grey -bg black'
 
     if uart_shell != 'false':
-        cmd_args = f"--attach_uart_cmd='{xterm_cmd} -T cerebri-shell -e screen %s &'"
+        uart_args = f"--attach_uart_cmd='{xterm_cmd} -T cerebri-shell -e screen %s &'"
     else:
-        cmd_args = ""
+        uart_args = ""
 
-    if debugger != 'false':
-        debug_prefix = f'{xterm_cmd} -T cerebri -e gdb -ex run --args'
+    if gdb != 'false':
+        debug_prefix = f'{xterm_cmd} -T cerebri -e gdb --args'
     else:
         debug_prefix = f'{xterm_cmd} -T cerebri -e'
 
     return [ExecuteProcess(
-            cmd=[cmd, cmd_args],
+            cmd=[cmd, uart_args],
             name='cerebri',
             prefix=debug_prefix,
             output='screen',
@@ -40,8 +40,8 @@ def generate_launch_description():
             choices=['true', 'false'],
             description='Run with uart shell.'),
         DeclareLaunchArgument(
-            'debugger', default_value='false',
-            description='Run in Debugger'),
+            'gdb', default_value='false',
+            description='Run in GDB Debugger'),
         DeclareLaunchArgument('vehicle',
             description='Vehicle to launch'),
         OpaqueFunction(function = launch_cerebri),
