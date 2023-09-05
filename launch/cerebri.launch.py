0170f695899301e42751792ec3ddcd8f06bfd2be
James Goppert <james.goppert@gmail.com>
2023-09-05 16:09:51 +0000

    Improve launch stability.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- launch/cerebri.launch.py
+++ launch/cerebri.launch.py
@@ -9,32 +9,30 @@ def launch_cerebri(context, *args, **kwargs):
 
     attach_uart = LaunchConfiguration('attach_uart').perform(context)
     gdb = LaunchConfiguration('gdb').perform(context)
-    xterm = LaunchConfiguration('xterm').perform(context)
     vehicle = LaunchConfiguration('vehicle').perform(context)
 
-    cerebri_exe = f"cerebri_{vehicle}_native_posix"
-    xterm_cmd = 'xterm -fa Monospace -fs 12 -fg grey -bg black'
+    cerebri_exe = [f"cerebri_{vehicle}_native_posix"]
+    xterm_cmd = 'xterm -fa Monospace -fs 12 -fg grey -bg black -T cerebri-gdb -e '
 
     if attach_uart != 'false':
-        uart_args = f"--attach_uart"
+        uart_args = ["--attach_uart"]
     else:
-        uart_args = ""
+        uart_args = []
 
     prefix = ''
-    if xterm != 'false':
-        prefix += f'{xterm_cmd} -T cerebri -e '
-
     if gdb != 'false':
-        prefix += 'gdb --args'
+        prefix += xterm_cmd + 'gdb --args '
 
-    cmd = f"{cerebri_exe} {uart_args}"
-    print(f'executing cmd: {cmd}')
+    cmd = cerebri_exe + uart_args
+    # print(f'executing cmd: {cmd}')
+    # print(f'prefix: {prefix}')
     return [ExecuteProcess(
-            cmd = [cmd],
+            cmd = cmd,
             name='cerebri',
             prefix=prefix,
             output='screen',
-            shell=True,
+            sigterm_timeout='1',
+            sigkill_timeout='1',
             on_exit=Shutdown()
             )]
 
@@ -47,9 +45,6 @@ def generate_launch_description():
         DeclareLaunchArgument(
             'gdb', default_value='false',
             description='Run in GDB Debugger'),
-        DeclareLaunchArgument(
-            'xterm', default_value='false',
-            description='Launch in xterm'),
         DeclareLaunchArgument('vehicle',
             description='Vehicle to launch'),
         OpaqueFunction(function = launch_cerebri),
